68dce6de5a30453be40f92a615aa0c54e1b9176f
GitHub Action <action@github.com>
2025-07-21 19:43:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/pr0ny/vuetemplatetester/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/pr0ny/vuetemplatetester/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.pr0ny.vuetemplatetester.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pr0ny.vuetemplatetester.MyBundle
+import com.github.pr0ny.vuetemplatetester.services.MyProjectService
 import javax.swing.JButton
 
 
